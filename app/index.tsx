f0ac69277ce2d71f2822154a0768e84d15784648
Sebastian Flor <sebastian.flor@atheneum.ai>
2024-06-26 12:53:30 +0200

    feat: finish welcome page

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,70 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import WelcomeImage from "@/assets/images/welcome.png";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
+
+const welcome_image = Image.resolveAssetSource(WelcomeImage).uri;
 
 export default function Page() {
+  const openLink = () => {};
   return (
     <View style={styles.container}>
-      <Text>First page</Text>
+      <Image source={{ uri: welcome_image }} style={styles.welcome} />
+      <Text style={styles.headline}>Welcome to whatsapp clone</Text>
+      <Text style={styles.description}>
+        Read our{" "}
+        <Text style={styles.link} onPress={openLink}>
+          Privacy Policy
+        </Text>
+        . {'Tap "Agree & Continue" to accept the '}
+        <Text style={styles.link} onPress={openLink}>
+          Terms of Service
+        </Text>
+        .
+      </Text>
+      <Link href={"/otp"} replace asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Agree & Continue</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  welcome: {
+    width: "100%",
+    height: 300,
+    marginBottom: 80,
+  },
+  headline: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginBottom: 80,
+    color: Colors.gray,
+  },
+  link: {
+    color: Colors.primary,
+  },
+  button: {
+    width: "100%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: Colors.primary,
+    fontSize: 22,
+    fontWeight: "bold",
+  },
 });
